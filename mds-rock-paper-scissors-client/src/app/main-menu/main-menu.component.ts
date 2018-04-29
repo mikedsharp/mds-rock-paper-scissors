65d137a9df3d48573e2725b144afc668153dd6ad
Mike Sharp <msharp23@hotmail.co.uk>
2018-04-29 14:34:06 +0100

    added lobby component and stub service

--- mds-rock-paper-scissors-client/src/app/main-menu/main-menu.component.ts
+++ mds-rock-paper-scissors-client/src/app/main-menu/main-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-menu',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  startGame() {
+    console.log('game start!');
+    this.router.navigate(['lobby']);
   }
 
 }
