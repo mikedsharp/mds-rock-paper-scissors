bff027e9462887714d3171bcc0e76c2b81bdf4a7
Mike Sharp <msharp23@hotmail.co.uk>
2018-04-29 14:17:45 +0100

    set up pages and routes

--- mds-rock-paper-scissors-client/src/app/app.module.ts
+++ mds-rock-paper-scissors-client/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { LobbyComponent } from './lobby/lobby.component';
+import { PlayAreaComponent } from './play-area/play-area.component';
 
+
+const appRoutes: Routes = [
+  {path: 'main-menu', component : MainMenuComponent},
+  {path: 'lobby', component : LobbyComponent},
+  {path: 'play-area', component : PlayAreaComponent},
+  {path: '**', component: MainMenuComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    LobbyComponent,
+    PlayAreaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
