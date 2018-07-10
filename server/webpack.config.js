622ec903bff6d47cf890a5966bd86555e1aea6d5
programmerinprogress <msharp23@hotmail.co.uk>
2018-07-10 21:10:53 +0100

    finally got sourcemaps working properly in vscode with webpack, not need for a separate build command!

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -13,9 +13,10 @@ module.exports = {
   entry: path.resolve(__dirname, 'src/server.ts'),
   output: {
     filename: 'server.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]'
   },
-  devtool: 'source-map',
+  devtool: 'eval-source-map',
   resolve: {
     // Add '.ts' and '.tsx' as a resolvable extension.
     extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
