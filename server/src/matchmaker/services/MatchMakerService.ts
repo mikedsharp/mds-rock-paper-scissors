890a58f9f466e9de8b1b9a460e7c52aec11ae0f6
programmerinprogress <msharp23@hotmail.co.uk>
2018-07-19 08:26:06 +0100

    further refactoring: moved subscription of listeners into separate functions. listener functions are now first class methods.

--- server/src/matchmaker/services/MatchMakerService.ts
+++ server/src/matchmaker/services/MatchMakerService.ts
@@ -86,87 +86,96 @@ export class MatchMakerService {
     const sessionIndex = this.sessions.indexOf(session);
     this.sessions.splice(sessionIndex, 1);
   }
-  private registerPlayerEventListeners(socket: socketIo.Socket) {
-    socket.on('answer-submitted', (data: any) => {
-      console.log('answer was submitted...', data);
-      const playerSessions = this.sessions.filter(session => {
-        return (
-          session.playerOne.socket === socket.id ||
-          session.playerTwo.socket === socket.id
-        );
-      });
-      if (playerSessions.length === 0) {
-        return;
-      }
-      playerSessions[0].registerPlayerMove(
-        this.players[socket.id].username,
-        data.move
+  private onAnswerSubmitted(socket: socketIo.Socket, data: any) {
+    console.log('answer was submitted...', data);
+    const playerSessions = this.sessions.filter(session => {
+      return (
+        session.playerOne.socket === socket.id ||
+        session.playerTwo.socket === socket.id
+      );
+    });
+    if (playerSessions.length === 0) {
+      return;
+    }
+    playerSessions[0].registerPlayerMove(
+      this.players[socket.id].username,
+      data.move
+    );
+
+    if (playerSessions[0].playersMovesSubmitted) {
+      const matchDecision = this.decisionMaker.getMatchDecision(
+        playerSessions[0]
+      );
+      this.io.sockets.connected[playerSessions[0].playerOne.socket].emit(
+        'match-decision',
+        {
+          matchDecision: matchDecision[playerSessions[0].playerOne.username],
+          opponentMove: playerSessions[0].playerTwo.move
+        }
+      );
+      this.io.sockets.connected[playerSessions[0].playerTwo.socket].emit(
+        'match-decision',
+        {
+          matchDecision: matchDecision[playerSessions[0].playerTwo.username],
+          opponentMove: playerSessions[0].playerOne.move
+        }
       );
 
-      if (playerSessions[0].playersMovesSubmitted) {
-        const matchDecision = this.decisionMaker.getMatchDecision(
-          playerSessions[0]
-        );
-        this.io.sockets.connected[playerSessions[0].playerOne.socket].emit(
-          'match-decision',
-          {
-            matchDecision: matchDecision[playerSessions[0].playerOne.username],
-            opponentMove: playerSessions[0].playerTwo.move
+      this.removeSession(playerSessions[0]);
+    }
+  }
+  private onRegisterPlayer(socket: socketIo.Socket, data: any) {
+    console.log('in register player...');
+    if (_.toArray(_.pickBy(this.players)).length === 0) {
+      console.log('starting poller...');
+      this.poller = setInterval(() => this.matchPlayers(), 5000);
+    }
+    this.players[socket.id] = new Player(data.username, socket.id);
+    this.io.sockets.connected[socket.id].emit('player-acknowledged');
+    console.log(`registered player: ${data.username}`);
+  }
+  private onPlayerDisconnected(socket: socketIo.Socket) {
+    console.log(`client disconnected`);
+    for (var socketId in this.players) {
+      if (this.players[socketId].socket === socket.id) {
+        console.log(`${this.players[socketId].username} disconnected`);
+        delete this.players[socketId];
+        const leavingPlayersSession = this.sessions.filter(session => {
+          return (
+            session.playerOne.socket === socketId ||
+            session.playerTwo.socket === socketId
+          );
+        });
+        if (leavingPlayersSession.length > 0) {
+          if (leavingPlayersSession[0].playerOne.socket === socketId) {
+            this.io.sockets.connected[
+              leavingPlayersSession[0].playerTwo.socket
+            ].emit('player-disconnected');
+          } else {
+            this.io.sockets.connected[
+              leavingPlayersSession[0].playerOne.socket
+            ].emit('player-disconnected');
           }
-        );
-        this.io.sockets.connected[playerSessions[0].playerTwo.socket].emit(
-          'match-decision',
-          {
-            matchDecision: matchDecision[playerSessions[0].playerTwo.username],
-            opponentMove: playerSessions[0].playerOne.move
-          }
-        );
+          this.removeSession(leavingPlayersSession[0]);
+        }
 
-        this.removeSession(playerSessions[0]);
+        if (_.toArray(_.pickBy(this.players)).length === 0) {
+          console.log('killing poller...');
+          clearInterval(this.poller);
+        }
+        break;
       }
+    }
+  }
+  private registerPlayerEventListeners(socket: socketIo.Socket) {
+    socket.on('answer-submitted', (data: any) => {
+      this.onAnswerSubmitted(socket, data);
     });
     socket.on('register-player', (data: any) => {
-      console.log('in register player...');
-      if (_.toArray(_.pickBy(this.players)).length === 0) {
-        console.log('starting poller...');
-        this.poller = setInterval(() => this.matchPlayers(), 5000);
-      }
-      this.players[socket.id] = new Player(data.username, socket.id);
-      this.io.sockets.connected[socket.id].emit('player-acknowledged');
-      console.log(`registered player: ${data.username}`);
+      this.onRegisterPlayer(socket, data);
     });
     socket.on('disconnect', () => {
-      console.log(`client disconnected`);
-      for (var socketId in this.players) {
-        if (this.players[socketId].socket === socket.id) {
-          console.log(`${this.players[socketId].username} disconnected`);
-          delete this.players[socketId];
-          const leavingPlayersSession = this.sessions.filter(session => {
-            return (
-              session.playerOne.socket === socketId ||
-              session.playerTwo.socket === socketId
-            );
-          });
-          if (leavingPlayersSession.length > 0) {
-            if (leavingPlayersSession[0].playerOne.socket === socketId) {
-              this.io.sockets.connected[
-                leavingPlayersSession[0].playerTwo.socket
-              ].emit('player-disconnected');
-            } else {
-              this.io.sockets.connected[
-                leavingPlayersSession[0].playerOne.socket
-              ].emit('player-disconnected');
-            }
-            this.removeSession(leavingPlayersSession[0]);
-          }
-
-          if (_.toArray(_.pickBy(this.players)).length === 0) {
-            console.log('killing poller...');
-            clearInterval(this.poller);
-          }
-          break;
-        }
-      }
+      this.onPlayerDisconnected(socket);
     });
   }
   private onPlayerConnect(socket: socketIo.Socket) {
